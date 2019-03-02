f811729004a36d51720532b085b7f3861dd13992
Robert Herber <rwherber@gmail.com>
2019-03-02 14:45:01 -0700

    more course done

---
+++ Start/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container">
+        <my-header></my-header>
+        <hr>
+        <div class="row">
+            <servers></servers>
+            <server-details></server-details>
+        </div>
+        <hr>
+        <my-footer></my-footer>
+    </div>
+</template>
+
+<script>
+import ServerDetails from './components/server/ServerDetails.vue'
+import Servers from './components/server/Servers.vue'
+import Header from './components/shared/Header.vue'
+import Footer from './components/shared/Footer.vue'
+
+export default 
+{
+    components:
+    {
+        'my-header': Header,
+        'my-footer': Footer,
+        'servers': Servers,
+        'server-details': ServerDetails
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-cli/src/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <app-server-status v-for="server in 5" :key="server.id"></app-server-status>
+    </div>
+  
+</template>
+
+<script>
+import ServerStatus from './ServerStatus.vue';
+
+export default 
+{
+  components: 
+  {
+      'app-server-status': ServerStatus
+  }
+}
+
+</script>
+
+<style>
+</style>
